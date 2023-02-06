5958264828c7d3e6c87d775634f838b73b68e8ea
danila <dakire@ttu.ee>
2023-02-06 23:04:58 +0200

    added throws

--- PR/PR02DataStructures/src/ee/taltech/iti0202/datastructures/DataStructures.java
+++ PR/PR02DataStructures/src/ee/taltech/iti0202/datastructures/DataStructures.java
@@ -1,6 +1,10 @@
 package ee.taltech.iti0202.datastructures;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DataStructures {
 
