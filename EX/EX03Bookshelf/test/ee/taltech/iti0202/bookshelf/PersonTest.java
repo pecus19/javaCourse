9e82c1b58fa4e3380ac83684f11a54f58605fca4
danila <dakire@ttu.ee>
2023-02-14 19:08:13 +0200

    bookshelf

--- EX/EX03Bookshelf/test/ee/taltech/iti0202/bookshelf/PersonTest.java
+++ EX/EX03Bookshelf/test/ee/taltech/iti0202/bookshelf/PersonTest.java
@@ -1,83 +1,85 @@
 package ee.taltech.iti0202.bookshelf;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 
 public class PersonTest {
+    public static final int SMALL = 1;
+    public static final int BIG = 100000;
+    public static final int NORMAL = 1000;
+    public static final int ID = 30;
     Person person3 = new Person("CR&", 1123233);
     Person person2 = new Person("CR&", 1234);
 
     @org.junit.jupiter.api.Test
     void getBooksByOwnerTest() {
         Book book99 = Book.of("Book", "Me", 2023, 100);
-        Book book98 = Book.of("Book", 100);
-        Book book97 = Book.of("Book", 100);
+        Book book98 = Book.of("Book", NORMAL);
+        Book book97 = Book.of("Book", NORMAL);
         if (book98 != null && book97 != null && book99 != null) {
             person3.buyBook(book97);
             person3.buyBook(book98);
             person3.buyBook(book99);
         }
-        assertEquals(3, Book.getBooksByOwner(person3).size());
+        Assertions.assertEquals(3, Book.getBooksByOwner(person3).size());
     }
 
     @org.junit.jupiter.api.Test
     void bookOfReturnNull2() {
-        assertEquals(null, Book.of("Book", 100));
+        Assertions.assertEquals(null, Book.of("Book", NORMAL));
     }
 
     @org.junit.jupiter.api.Test
     void bookBuyReturnNullWithBuyerNull() {
-        Book b1 = Book.of("Java EX01", "Ago Luberg", 2018, 3);
-        Person bonusPerson = new Person("Joonas Boonus", 10000);
-        Person bonusPerson2 = new Person("Joonas 2", 100);
-        Book harry1 = Book.of("Harry Potter: The Philosopher's Stone", "J. K. rowling", 1997, 1000);
+        Book b1 = Book.of("Java EX01", "Ago Luberg", BIG, SMALL);
+        Person bonusPerson = new Person("Joonas Boonus", BIG);
+        Person bonusPerson2 = new Person("Joonas 2", NORMAL);
+        Book harry1 = Book.of("Harry Potter: The Philosopher's Stone", "J. K. rowling", BIG, BIG);
         if (b1 != null) {
             b1.buy(bonusPerson2);
-            assertTrue(b1.buy(null));
+            Assertions.assertTrue(b1.buy(null));
         }
     }
 
     @org.junit.jupiter.api.Test
     void bookBuyReturnNullWithBuyerNullFalse() {
-        Book b1 = Book.of("Java EX01", "Ago Luberg", 2018, 3);
-        Person bonusPerson = new Person("Joonas Boonus", 1);
-        Person bonusPerson2 = new Person("Joonas 2", 100);
+        Book b1 = Book.of("Java EX01", "Ago Luberg", BIG, SMALL);
+        Person bonusPerson = new Person("Joonas Boonus", SMALL);
+        Person bonusPerson2 = new Person("Joonas 2", NORMAL);
         b1.buy(bonusPerson2);
-        assertFalse(b1.buy(bonusPerson));
+        Assertions.assertFalse(b1.buy(bonusPerson));
     }
 
     @org.junit.jupiter.api.Test
     void bookOfWithNullAuthor() {
-        Book java3 = Book.of("Java EX01", 3);
-        assertNull(java3);
+        Book java3 = Book.of("Java EX01", SMALL);
+        Assertions.assertNull(java3);
     }
 
     @org.junit.jupiter.api.Test
     void removeBookThatDoesNotExist() {
-        Book java3 = Book.of("Java EX01", 3);
-        assertFalse(Book.removeBook(java3));
+        Book java3 = Book.of("Java EX01", SMALL);
+        Assertions.assertFalse(Book.removeBook(java3));
     }
 
     @org.junit.jupiter.api.Test
     void bookOfAddSameBookReturnNull() {
-        Book b1 = Book.of("Java2023", "Aga", 2018, 3);
-        assertNull(Book.of("Java2023", "Aga", 2018, 3));
+        Book b1 = Book.of("Java2023", "Aga", NORMAL, SMALL);
+        Assertions.assertNull(Book.of("Java2023", "Aga", NORMAL, SMALL));
     }
 
     @org.junit.jupiter.api.Test
     void removeBookTrue() {
-        Book b172 = Book.of("b172", "Den12", 2018, 3);
-        Person bonus21 = new Person("21", 10920);
+        Book b172 = Book.of("b172", "Den12", NORMAL, SMALL);
+        Person bonus21 = new Person("21", BIG);
         b172.buy(bonus21);
-        assertTrue(Book.removeBook(b172));
+        Assertions.assertTrue(Book.removeBook(b172));
     }
     @org.junit.jupiter.api.Test
     void sellBookWithoutBuyingFalse() {
-        Book b172 = Book.of("b172", "Den12", 2018, 3);
-        Person bonus31 = new Person("31", 1);
+        Book b172 = Book.of("b172", "Den12", NORMAL, BIG);
+        Person bonus31 = new Person("31", SMALL);
         bonus31.sellBook(b172);
-        assertEquals("31", bonus31.getName());
-        assertFalse(bonus31.sellBook(b172));
+        Assertions.assertEquals("31", bonus31.getName());
+        Assertions.assertFalse(bonus31.sellBook(b172));
     }
-
-
 }
