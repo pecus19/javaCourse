af489d4b96942efcee89afba1e194034b5b84336
danila <dakire@ttu.ee>
2023-02-21 20:03:34 +0200

    100

--- PR/PR04Stock/src/ee/taltech/iti0202/stock/stock/Stock.java
+++ PR/PR04Stock/src/ee/taltech/iti0202/stock/stock/Stock.java
@@ -56,8 +56,7 @@ public class Stock {
 
     public void addProduct(Product product) throws StockException {
         if (!products.contains(product)) {
-            int check = numOfProducts += ONE;
-            if (isFull()) {
+            if (numOfProducts != maxCapacity) {
                 products.add(product);
                 totalPrice += product.getPrice();
             } else {
