6a95aa4d076108c168434240d006dfb384fe761b
danila <dakire@ttu.ee>
2023-02-28 18:08:16 +0200

    stream

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/SpaceOven.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/SpaceOven.java
@@ -49,13 +49,13 @@ public class SpaceOven extends Oven implements Fixable {
             balls++;
             return Optional.of(orb);
         } else {
-            if (getResourceStorage().hasEnoughResource("pearl", 1)) {
-                if (getResourceStorage().hasEnoughResource("silver", 1)) {
+            if (getResourceStorage().hasEnoughResource("meteorite stone", 1)) {
+                if (getResourceStorage().hasEnoughResource("star fragment", INT)) {
                     Orb orb = new Orb(getName());
-                    getResourceStorage().takeResource("pearl", 1);
-                    getResourceStorage().takeResource("silver", 1);
-                    orb.charge("pearl", 1);
-                    orb.charge("silver", 1);
+                    getResourceStorage().takeResource("meteorite stone", 1);
+                    getResourceStorage().takeResource("star fragment", INT);
+                    orb.charge("meteorite stone", 1);
+                    orb.charge("star fragment", INT);
                     balls++;
                     return Optional.of(orb);
                 }
