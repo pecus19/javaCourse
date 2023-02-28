6ace9ad6995bc629ee054fc1fd931617286c699e
danila <dakire@ttu.ee>
2023-02-28 18:05:31 +0200

    stream

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/SpaceOven.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/SpaceOven.java
@@ -30,14 +30,14 @@ public class SpaceOven extends Oven implements Fixable {
 
     @Override
     public Optional<Orb> craftOrb() {
-        if (isBroken()) {
-            try {
-                fix(); // Call the fix() method to attempt to fix the orb
-            } catch (CannotFixException e) {
-//            System.out.println("Cannot craft orb: " + e.getMessage());
-//            return Optional.empty();
-            }
-        }
+//        if (isBroken()) {
+//            try {
+//                fix(); // Call the fix() method to attempt to fix the orb
+//            } catch (CannotFixException e) {
+////            System.out.println("Cannot craft orb: " + e.getMessage());
+////            return Optional.empty();
+//            }
+//        }
         if (!isBroken() && !getResourceStorage().isEmpty()
                 && getResourceStorage().hasEnoughResource("meteorite stone", 1)
                 && getResourceStorage().hasEnoughResource("star fragment", INT)) {
@@ -49,13 +49,13 @@ public class SpaceOven extends Oven implements Fixable {
             balls++;
             return Optional.of(orb);
         } else {
-            if (getResourceStorage().hasEnoughResource("meteorite stone", 1)) {
-                if (getResourceStorage().hasEnoughResource("star fragment", INT)) {
+            if (getResourceStorage().hasEnoughResource("pearl", 1)) {
+                if (getResourceStorage().hasEnoughResource("silver", 1)) {
                     Orb orb = new Orb(getName());
-                    getResourceStorage().takeResource("meteorite stone", 1);
-                    getResourceStorage().takeResource("star fragment", INT);
-                    orb.charge("meteorite stone", 1);
-                    orb.charge("star fragment", INT);
+                    getResourceStorage().takeResource("pearl", 1);
+                    getResourceStorage().takeResource("silver", 1);
+                    orb.charge("pearl", 1);
+                    orb.charge("silver", 1);
                     balls++;
                     return Optional.of(orb);
                 }
@@ -91,5 +91,4 @@ public class SpaceOven extends Oven implements Fixable {
     public int getTimesFixed() {
         return times;
     }
-
 }
