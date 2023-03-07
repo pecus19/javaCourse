4c83c9c77f06da6fe99820811bf24c67969cd49f
danila <dakire@ttu.ee>
2023-03-07 12:29:36 +0200

    xz

--- EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/morse/MorseTranslator.java
@@ -69,7 +69,11 @@ public class MorseTranslator {
             char char1 = line.charAt(i);
             String str = String.valueOf(char1).toLowerCase();
             if (str.equals(" ")) {
-                output = new StringBuilder(output.substring(0, output.length()) + "\t");
+                if (output.length() - 1 != i) {
+                    output = new StringBuilder(output.substring(0, output.length()) + "\t");
+                } else {
+                    output = new StringBuilder(output.substring(0, output.length()));
+                }
             } else {
                 output.append(morseMap.get(str));
                 if (output.length() - 1 != i) {
