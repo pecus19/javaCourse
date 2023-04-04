c079ccab52e81e64c2952b23ec932e73ff381966
danila <dakire@ttu.ee>
2023-04-04 09:43:05 +0300

    delivery

--- EX/EX10Delivery/src/ee/taltech/iti0202/delivery/Packet.java
+++ EX/EX10Delivery/src/ee/taltech/iti0202/delivery/Packet.java
@@ -4,6 +4,14 @@ public class Packet {
     private String name;
     private Location target;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTarget(Location target) {
+        this.target = target;
+    }
+
     public Packet(String name, Location target) {
         this.name=name;
         this.target=target;
