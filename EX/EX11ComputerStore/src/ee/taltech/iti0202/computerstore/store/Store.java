90cacd0a8258cd86255afe140cfe7ad397ab67d2
danila <dakire@ttu.ee>
2023-04-25 00:10:51 +0300

    ex 11 first commit

--- EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/store/Store.java
+++ EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/store/Store.java
@@ -31,14 +31,24 @@ public class Store {
     }
 
     public Store(String name, BigDecimal balance, BigDecimal profitMargin) {
-        BigDecimal one = new BigDecimal("1");
-        if (profitMargin.compareTo(one) > -1) {
+//        BigDecimal one = new BigDecimal("1");
+//        if (profitMargin.compareTo(one) > -1) {
+//            throw new IllegalArgumentException();
+//        }
+//        this.name = name;
+//        this.balance = balance;
+//        this.profitMargin = profitMargin;
+//        this.database = Database.getInstance();
+        BigDecimal bg1 = new BigDecimal("1");
+        int res = profitMargin.compareTo(bg1);
+        if (res < 0) {
             throw new IllegalArgumentException();
+        } else {
+            this.name = name;
+            this.balance = balance;
+            this.profitMargin = profitMargin;
+            this.database = Database.getInstance();
         }
-        this.name = name;
-        this.balance = balance;
-        this.profitMargin = profitMargin;
-        this.database = Database.getInstance();
     }
 
     public Component purchaseComponent(int id, Customer customer) throws OutOfStockException,
