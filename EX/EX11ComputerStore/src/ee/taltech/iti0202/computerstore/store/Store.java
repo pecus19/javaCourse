fa747ff3224fa32a5425c3bbd0de361104cd2016
danila <dakire@ttu.ee>
2023-04-25 00:23:38 +0300

    ex 11 first commit

--- EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/store/Store.java
+++ EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/store/Store.java
@@ -7,8 +7,6 @@ import ee.taltech.iti0202.computerstore.exceptions.NotEnoughMoneyException;
 import ee.taltech.iti0202.computerstore.exceptions.OutOfStockException;
 import ee.taltech.iti0202.computerstore.exceptions.ProductNotFoundException;
 
-import java.math.MathContext;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.math.BigDecimal;
@@ -17,118 +15,97 @@ import java.util.stream.Collectors;
 public class Store {
     private String name;
     private BigDecimal balance;
-    private final int six = 6;
-
-
     private BigDecimal profitMargin;
     private final Database database;
 
-    public Store(String name, BigDecimal balance, BigDecimal profitMargin) {
-        BigDecimal bg1 = new BigDecimal("1");
-        int res = profitMargin.compareTo(bg1);
-        if (res < 0) {
-            throw new IllegalArgumentException();
-        } else {
-            this.name = name;
-            this.balance = balance;
-            this.profitMargin = profitMargin;
-            this.database = Database.getInstance();
-        }
-    }
-
-
-    public Component purchaseComponent(int id, Customer customer) throws OutOfStockException,
-            ProductNotFoundException,
-            NotEnoughMoneyException {
-        if (database.getComponents().containsKey(id)) {
-            Component component = database.getComponents().get(id);
-            BigDecimal newPrice = profitMargin.multiply(component.getPrice());
-            if (newPrice.compareTo(customer.getBalance()) > 0) {
-                throw new NotEnoughMoneyException();
-            } else {
-                database.decreaseComponentStock(component.getId(), 1);
-                balance = balance.add(newPrice);
-                customer.setBalance(customer.getBalance().subtract(newPrice));
-                customer.addComponents(component);
-                return component;
-
-            }
-        }
-        return null;
-    }
-
-    public List<Component> getAvailableComponents() {
-        return database.getComponents()
-                .values()
-                .stream()
-                .filter(c -> c.getAmount() > 0)
-                .collect(Collectors.toList());
-    }
-
-    public List<Component> getComponentsSortedByAmount() {
-        return new ArrayList<>(database.getComponents().values())
-                .stream()
-                .sorted(Comparator.comparingInt(Component::getAmount))
-                .collect(Collectors.toList());
-    }
-
-    public List<Component> getComponentsSortedByName() {
-        return new ArrayList<>(database.getComponents().values())
-                .stream()
-                .sorted(Comparator.comparing(Component::getName))
-                .collect(Collectors.toList());
-    }
-
-    public List<Component> getComponentsSortedByPrice() {
-        return new ArrayList<>(database.getComponents().values())
-                .stream()
-                .sorted(Comparator.comparing(Component::getPrice))
-                .collect(Collectors.toList());
-    }
-
-    public List<Component> filterByType(Component.Type type) {
-        return new ArrayList<>(database.getComponents().values())
-                .stream()
-                .filter(o -> o.getType().equals(type))
-                .collect(Collectors.toList());
-    }
-
-    public BigDecimal getInventoryValue() {
-        BigDecimal output = BigDecimal.ZERO;
-        for (Component component : database.getComponents().values()) {
-            BigDecimal num1 = new BigDecimal(component.getAmount());
-            output = output.add(component.getPrice().multiply(num1).multiply(profitMargin));
-        }
-        MathContext mc = new MathContext(six);
-        return output.round(mc);
-    }
-
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
     }
 
     public void setBalance(BigDecimal balance) {
         this.balance = balance;
     }
 
-    public BigDecimal getProfitMargin() {
-        return profitMargin;
+    public void setProfitMargin(BigDecimal profitMargin) {
+        this.profitMargin = profitMargin;
     }
 
-    public void setProfitMargin(BigDecimal profitMargin) {
-        BigDecimal bg1 = new BigDecimal("1");
-        int res = profitMargin.compareTo(bg1);
-        if (res < 0) {
+    public Store(String name, BigDecimal balance, BigDecimal profitMargin) {
+        BigDecimal one = new BigDecimal("1");
+        if (profitMargin.compareTo(one) < 1) {
             throw new IllegalArgumentException();
         }
+        this.name = name;
+        this.balance = balance;
         this.profitMargin = profitMargin;
+        this.database = Database.getInstance();
+    }
+
+    public Component purchaseComponent(int id, Customer customer) throws OutOfStockException,
+            ProductNotFoundException,
+            NotEnoughMoneyException {
+        Component component;
+        if (!database.getComponents().containsKey(id)) {
+            throw new ProductNotFoundException();
+        }
+        component = database.getComponents().get(id);
+        BigDecimal price = profitMargin.multiply(component.getPrice());
+        if (price.compareTo(customer.getBalance()) > 0) {
+            throw new NotEnoughMoneyException();
+        }
+        customer.addComponents(component);
+        database.deleteComponent(id);
+        balance = balance.add(price);
+        customer.setBalance(customer.getBalance().subtract(price));
+        return component;
+    }
+
+    public List<Component> getAvailableComponents() {
+        return database.getComponents().values().stream()
+                .filter(component -> component.getAmount() > 0)
+                .collect(Collectors.toList());
+
+    }
+
+    public List<Component> getComponentsSortedByAmount() {
+        return database.getComponents().values().stream()
+                .sorted(Comparator.comparing(Component::getAmount))
+                .collect(Collectors.toList());
+    }
+
+    public List<Component> getComponentsSortedByName() {
+        return database.getComponents().values().stream()
+                .sorted(Comparator.comparing(Component::getName))
+                .collect(Collectors.toList());
+    }
+
+    public List<Component> getComponentsSortedByPrice() {
+        return database.getComponents().values().stream()
+                .sorted(Comparator.comparing(Component::getPrice))
+                .collect(Collectors.toList());
+    }
+
+    public List<Component> filterByType(Component.Type type) {
+        return database.getComponents().values().stream()
+                .filter(component -> component.getType()
+                        .equals(type)).toList();
+    }
+
+    public BigDecimal getInventoryValue() {
+        return BigDecimal.ZERO;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+
+    public BigDecimal getProfitMargin() {
+        return profitMargin;
     }
 }
