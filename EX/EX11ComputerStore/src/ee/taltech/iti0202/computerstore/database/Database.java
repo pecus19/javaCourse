1b0aca14b08ca062342a567ff5cbb980ce95d0d1
danila <dakire@ttu.ee>
2023-04-25 00:00:39 +0300

    ex 11 first commit

--- EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
+++ EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
@@ -1,13 +1,21 @@
 package ee.taltech.iti0202.computerstore.database;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import ee.taltech.iti0202.computerstore.components.Component;
 import ee.taltech.iti0202.computerstore.exceptions.OutOfStockException;
 import ee.taltech.iti0202.computerstore.exceptions.ProductAlreadyExistsException;
 import ee.taltech.iti0202.computerstore.exceptions.ProductNotFoundException;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Database {
     private final Map<Integer, Component> components = new HashMap<>();
@@ -80,8 +88,27 @@ public class Database {
     }
 
     public void saveToFile(String location) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Path path = Paths.get(location);
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            gson.toJson(this, writer);
+        } catch (IOException e) {
+            System.out.println("IOException:" + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     public void loadFromFile(String location) {
+        Gson gson = new Gson();
+        Path path = Paths.get(location);
+        try (Scanner scanner = new Scanner(path)) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                instance = gson.fromJson(line, Database.class);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException:" + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
