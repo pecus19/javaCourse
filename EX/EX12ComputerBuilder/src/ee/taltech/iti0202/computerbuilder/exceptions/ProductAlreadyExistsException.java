ec817314cb6d648eec2919089d15f650e973d966
danila <dakire@ttu.ee>
2023-05-02 00:44:52 +0300

    second commit

--- EX/EX12ComputerBuilder/src/ee/taltech/iti0202/computerbuilder/exceptions/ProductAlreadyExistsException.java
+++ EX/EX12ComputerBuilder/src/ee/taltech/iti0202/computerbuilder/exceptions/ProductAlreadyExistsException.java
@@ -1,4 +1,12 @@
 package ee.taltech.iti0202.computerbuilder.exceptions;
 
 public class ProductAlreadyExistsException extends Exception {
+    public static final String MESSAGE = "Product already exists exception!";
+
+    /**
+     * constructor
+     */
+    public ProductAlreadyExistsException() {
+        super(MESSAGE);
+    }
 }
