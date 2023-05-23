73fbc07800acc4f7d4df31d76ee71e442d29c71d
dakire <dakire@ttu.ee>
2023-05-23 10:22:39 +0300

    smt

--- EXAM/EXAM1/src/ee/taltech/iti0202/exam/library/Book.java
+++ EXAM/EXAM1/src/ee/taltech/iti0202/exam/library/Book.java
@@ -3,18 +3,22 @@ package ee.taltech.iti0202.exam.library;
 import java.util.Optional;
 
 public class Book {
+    private String title;
+    private String isbn;
     /**
      * Creates a new book with the given title and ISBN.
      */
     public Book(String title, String isbn) {
+        this.title = title;
+        this.isbn = isbn;
     }
 
     public String getTitle() {
-        return null;
+        return title;
     }
 
     public String getIsbn() {
-        return null;
+        return isbn;
     }
 
     public static void main(String[] args) {
--- EXAM/EXAM1/src/ee/taltech/iti0202/exam/library/Library.java
+++ EXAM/EXAM1/src/ee/taltech/iti0202/exam/library/Library.java
@@ -1,27 +1,37 @@
 package ee.taltech.iti0202.exam.library;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 public class Library {
+    List<Book> books = new ArrayList<>();
+
     /**
      * Adds a book to the library.
-     *
+     * <p>
      * If the same book (same instance) is already in the library, return false.
      * Otherwise the book is added to the library and return true.
      */
     public boolean addBook(Book book) {
-        return false;
+        if (books.contains(book)) {
+            return false;
+        } else {
+            books.add(book);
+            return true;
+        }
     }
+
 
     /**
      * Tries to lend a book.
-     *
+     * <p>
      * If there is an available book with the exact name, then this book is returned.
      * Otherwise, if there is an available book where the title contains the name,
      * then this book is returned.
      * Otherwise, nothing is returned.
-     *
+     * <p>
      * If there is a book, then this book will not be available any more (it is lent out).
      */
     public Optional<Book> lendBook(String name) {
@@ -30,10 +40,10 @@ public class Library {
 
     /**
      * Returns a book.
-     *
+     * <p>
      * If the given book is lent from the library, returns it and returns true.
      * Otherwise returns false.
-     *
+     * <p>
      * After returning, the book can be lent again.
      */
     public boolean returnBook(Book book) {
@@ -49,7 +59,7 @@ public class Library {
 
     /**
      * Returns how many times the book has been lent.
-     *
+     * <p>
      * If the book is not in the library, returns -1.
      */
     public int getBookLendCount(Book book) {
