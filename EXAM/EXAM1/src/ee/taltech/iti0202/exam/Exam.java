6b019cd1856ba5292e299393db2a61cc8597cabf
dakire <dakire@ttu.ee>
2023-05-23 10:44:54 +0300

    smt

--- EXAM/EXAM1/src/ee/taltech/iti0202/exam/Exam.java
+++ EXAM/EXAM1/src/ee/taltech/iti0202/exam/Exam.java
@@ -29,7 +29,7 @@ public class Exam {
         int output = 0;
         int newNum = num;
         for (int i = 8; i < 100000; i++) {
-            if (i % 2 != 0 || i % 3 != 0 || i % 5 != 0 || i % 7 != 0) {
+            if (i % 2 != 0 | i % 3 != 0 | i % 5 != 0 | i % 7 != 0) {
                 primeNumbers.add(i);
             }
         }
